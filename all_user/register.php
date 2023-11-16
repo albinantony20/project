d744a99ebd0781df2b41c7e34ad74ed516bfda8a
albinantony20 <albinantonymgm@gmail.com>
2023-11-16 13:17:13 +0530

    Aa

--- all_user/register.php
+++ all_user/register.php
@@ -97,7 +97,7 @@ if(isset($_POST['signup']))
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term"required/>
-                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a> </label>
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in Terms of service</a> </label>
                             </div>
                             <div class="form-group form-button">
                                 <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
